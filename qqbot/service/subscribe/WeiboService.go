20e758a4474d99b537dc2801ee4c7489440af862
mapleFU <1506118561@qq.com>
2019-09-05 02:21:50 +0800

    [Update] Update logic of weibo-service.

--- qqbot/service/subscribe/WeiboService.go
+++ qqbot/service/subscribe/WeiboService.go
@@ -1,16 +1,15 @@
 package subscribe
 
 import (
+	"sort"
+	"strings"
+	"time"
+
 	"github.com/mapleFU/QQBot/qqbot/data/group"
 	"github.com/mapleFU/QQBot/qqbot/logger"
 	"github.com/mapleFU/QQBot/qqbot/service"
-	"time"
 
-	"bytes"
-	"golang.org/x/net/html"
-	"log"
-	"strings"
-
+	"github.com/PuerkitoBio/goquery"
 	"github.com/grokify/html-strip-tags-go"
 	"github.com/mmcdole/gofeed"
 )
@@ -20,29 +19,48 @@ type WeiboService struct {
 	ServiceUrl string
 }
 
-func removeScript(n *html.Node) {
-	// if note is script tag
-	if n.Type == html.ElementNode && (n.Data == "a" || n.Data == "img" || n.Data == "br" || n.Data == "body" || n.Data == "html") {
-		n.Parent.RemoveChild(n)
-		return // script tag is gone...
-	}
-	// traverse DOM
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		removeScript(c)
-	}
-}
+//func removeScript(n *html.Node) {
+//	// if note is script tag
+//	if n.Type == html.ElementNode && (n.Data == "a" || n.Data == "img" || n.Data == "br" || n.Data == "body" || n.Data == "html") {
+//		n.Parent.RemoveChild(n)
+//		return // script tag is gone...
+//	}
+//	// traverse DOM
+//	for c := n.FirstChild; c != nil; c = c.NextSibling {
+//		removeScript(c)
+//	}
+//}
+//
+//func extractLink(linkText string) string {
+//	doc, err := html.Parse(strings.NewReader(linkText))
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	removeScript(doc)
+//	buf := bytes.NewBuffer([]byte{})
+//	if err := html.Render(buf, doc); err != nil {
+//		log.Fatal(err)
+//	}
+//	return buf.String()
+//}
 
-func extractLink(linkText string) string {
-	doc, err := html.Parse(strings.NewReader(linkText))
+// Get image from web link
+// TODO: move this function to html utils
+func ExtractImages(rawDoc string) ([]string, error) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(rawDoc))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	removeScript(doc)
-	buf := bytes.NewBuffer([]byte{})
-	if err := html.Render(buf, doc); err != nil {
-		log.Fatal(err)
-	}
-	return buf.String()
+
+	links := make([]string, 0)
+	doc.Find("img").Each(func(_ int, selection *goquery.Selection) {
+		val, exists := selection.Attr("src")
+		if exists {
+			links = append(links, val)
+		}
+	})
+	logger.SLogger.Info(links)
+	return links, nil
 }
 
 func NewWeiboService(weiboUrl string) *WeiboService {
@@ -63,8 +81,6 @@ func buildService(item *gofeed.Item, title string) group.StringRespMessage {
 	return Resp
 }
 
-// TODO: make clear how it end.
-// TODO: consider using redis or other persistence techniques
 func (self *WeiboService) Run() {
 	fp := gofeed.NewParser()
 	var lastNewest *gofeed.Item
@@ -72,40 +88,53 @@ func (self *WeiboService) Run() {
 
 	// 考虑任务如何中止
 	for {
-		// 10 分钟一次
-		feed, _ := fp.ParseURL(self.ServiceUrl)
-		if feed.Items == nil {
-			logger.SLogger.Info("Feed.Items is nil!")
-		}
-		if lastNewest == nil {
-			lastNewest = feed.Items[0]
-			logger.SLogger.Info("LastNewest Inited!")
+		func() {
 
-			//title = "[测试消息]" + lastNewest.Title
-			//Resp := buildService(lastNewest, title)
-			//logger.SLogger.Info("ready to send resp ", "resp", Resp)
-			//*self.OutChan <- Resp
-		}
-
-		for _, item := range feed.Items {
-			if item == nil {
-				logger.SLogger.Info("item is nil here")
+			// 10 分钟一次
+			feed, err := fp.ParseURL(self.ServiceUrl)
+			if err != nil {
+				logger.SLogger.Info("fp.ParseURL(self.ServiceUrl) failed with reason ", "error", err.Error())
+				time.Sleep(time.Second * 5)
+				return
 			}
 
-			if item.Title == lastNewest.Title {
-				logger.SLogger.Info("item.Title == lastNewest.Title")
-				// set latest news to the newer latest field
+			defer time.Sleep(time.Minute * 5)
+
+			if feed.Items == nil {
+				logger.SLogger.Info("Feed.Items is nil!")
+				return
+			}
+			sort.Slice(feed.Items, func(i, j int) bool {
+				return feed.Items[i].PublishedParsed.After(*feed.Items[j].PublishedParsed)
+			})
+
+			// initialize lastNewest
+			// 指向 feed.Items[0]
+			if lastNewest == nil {
 				lastNewest = feed.Items[0]
-				break
-			} else {
-
-				title = item.Title
-				Resp := buildService(item, title)
-				logger.SLogger.Info("ready to send resp ", "resp", Resp)
-				*self.OutChan <- Resp
+				logger.SLogger.Info("LastNewest Inited!")
+				return
 			}
-		}
-		// TODO: debug set it 1
-		time.Sleep(time.Minute * 5)
+
+			for _, item := range feed.Items {
+				// item 是 nil
+				if item == nil {
+					logger.SLogger.Info("item is nil here")
+					continue
+				}
+
+				if item.Title == lastNewest.Title {
+					logger.SLogger.Info("item.Title == lastNewest.Title")
+					// set latest news to the newer latest field
+					lastNewest = feed.Items[0]
+					break
+				} else {
+					title = item.Title
+					Resp := buildService(item, title)
+					logger.SLogger.Info("ready to send resp ", "resp", Resp)
+					*self.OutChan <- Resp
+				}
+			}
+		}()
 	}
 }
